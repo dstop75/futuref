64557d149b3c76ae53489a0e8ec46882756d9041
Dan Stoppelman <dstop75@gmail.com>
2015-02-08 18:27:52 -0500

    pass unit: GET /topics/:topic_id/resources routes to resources#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'topics#index'
   resources :topics, shallow: true do
-    resources :resources, only: [:new]
+    resources :resources, only: [:index, :new]
   end
 end
