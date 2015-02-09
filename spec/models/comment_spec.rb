90e84dd0abb06dbeec16ac29a703d6b38df302dc
Dan Stoppelman <dstop75@gmail.com>
2015-02-09 12:29:57 -0500

    pass unit: Comment.create is valid with a message and a resource_id

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -17,8 +17,8 @@ RSpec.describe Comment do
       expect(Comment.create(message: 'Deploying to Heroku')).not_to be_valid
     end
 
-    # it 'is valid with a message and a resource_id' do
-    #   expect(Comment.create(message: 'Deploying to Heroku', resource: resource)).to be_valid
-    # end
+    it 'is valid with a message and a resource_id' do
+      expect(Comment.create(message: 'Deploying to Heroku', resource: resource)).to be_valid
+    end
   end
 end
