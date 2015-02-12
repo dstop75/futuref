cac7961010566943902dfd8359464ca07161502b
Dan Stoppelman <dstop75@gmail.com>
2015-02-11 19:38:32 -0500

    skinny up the CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,36 +1,37 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: [:edit, :update, :destroy]
+  before_action :set_topic, only: [:update, :destroy]
 
   def edit
     @resource = @comment.resource
   end
 
   def create
-    @resource = Resource.find(params[:resource_id])
-    @comment = @resource.comments.new(comment_params)
+    resource = Resource.find(params[:resource_id])
+    @comment = resource.comments.new(comment_params)
 
     flash[:success] = 'Comment successfully created.' if @comment.save
 
-    redirect_to @resource.topic
+    redirect_to resource.topic
   end
 
   def update
-    @resource = @comment.resource
-    @topic = @resource.topic
-
     flash[:success] = 'Comment successfully updated.' if @comment.update(comment_params)
 
     redirect_to @topic
   end
 
   def destroy
-    @topic = @comment.resource.topic
-    @comment.destroy
-    flash[:success] = 'Comment successfully deleted.'
+    flash[:success] = 'Comment successfully deleted.' if @comment.destroy
+
     redirect_to @topic
   end
 
   private
+    def set_topic
+      @topic = @comment.resource.topic
+    end
+
     def set_comment
       @comment = Comment.find(params[:id])
     end
