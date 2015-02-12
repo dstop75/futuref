d296ee02b3dc657b569fb44d96f3db0053b43ca0
Dan Stoppelman <dstop75@gmail.com>
2015-02-11 20:03:48 -0500

    skinny up the ResourcesController

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,16 +1,16 @@
 class ResourcesController < ApplicationController
+  before_action :set_topic, only: [:new, :create]
+  before_action :set_resource, only: [:edit, :update, :destroy]
+
   def new
     @topics = Topic.all
-    @topic = Topic.find(params[:topic_id])
     @resource = @topic.resources.new
   end
 
   def edit
-    @resource = Resource.find(params[:id])
   end
 
   def create
-    @topic = Topic.find(params[:topic_id])
     @resource = @topic.resources.new(resource_params)
 
     if @resource.save
@@ -22,25 +22,34 @@ class ResourcesController < ApplicationController
   end
 
   def update
-    @resource = Resource.find(params[:id])
-    @topic = @resource.topic
+    topic = @resource.topic
 
     if @resource.update(resource_params)
       flash[:success] = 'Link successfully updated.'
-      redirect_to @topic
+      redirect_to topic
     else
       render :edit
     end
   end
 
   def destroy
-    @topic = Resource.find(params[:id]).topic
-    Resource.find(params[:id]).destroy
+    topic = @resource.topic
+    @resource.destroy
+
     flash[:success] = 'Link successfully deleted.'
-    redirect_to @topic
+
+    redirect_to topic
   end
 
   private
+    def set_resource
+      @resource = Resource.find(params[:id])
+    end
+
+    def set_topic
+      @topic = Topic.find(params[:topic_id])
+    end
+
     def resource_params
       params.require(:resource).permit(:name, :url)
     end
