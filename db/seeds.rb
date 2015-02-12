a02ba94a39bd85e2f6cec8e0c97d685da9c48c4f
Dan Stoppelman <dstop75@gmail.com>
2015-02-11 23:42:56 -0500

    edit seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Topic.delete_all
 heroku = Topic.create!(name: 'Heroku')
 rails = Topic.create!(name: 'Rails')
 rest = Topic.create!(name: 'REST')
 bootstrap = Topic.create!(name: 'Bootstrap')
 
-Resource.delete_all
 link_1 = heroku.resources.create!(name: 'Deploying To Heroku', url: 'http://docs.railsbridge.org/intro-to-rails/deploying_to_heroku')
 link_2 = heroku.resources.create!(name: 'Getting Started With Rails', url: 'https://devcenter.heroku.com/articles/getting-started-with-rails4')
 
-Comment.delete_all
 link_1.comments.create!(message: 'go-to link')
 link_2.comments.create!(message: 'super helpful')
